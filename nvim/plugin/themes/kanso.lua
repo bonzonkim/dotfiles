a39e84c7b4699a8fea0039ce21d0aec6634114bb
Bumgu Kang <flrhyme9@gmail.com>
2025-05-08 09:13:16 +0900

    update: theme from kanagawa to nightfox

--- nvim/plugin/themes/kanso.lua
+++ nvim/plugin/themes/kanso.lua
@@ -1,30 +1,31 @@
---Default options:
---local status, kanagawa = pcall(require, 'kanagawa')
---if not status then return end
---kanagawa.setup({
+--local status, kanso = pcall(require, "kanso")
+--if (not status) then return end
+---- Default options:
+--kanso.setup({
 --    compile = false,             -- enable compiling the colorscheme
 --    undercurl = true,            -- enable undercurls
 --    commentStyle = { italic = true },
 --    functionStyle = {},
 --    keywordStyle = { italic = true},
---    statementStyle = { bold = true },
+--    statementStyle = {},
 --    typeStyle = {},
+--    disableItalics = false,
 --    transparent = false,         -- do not set background color
 --    dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
 --    terminalColors = true,       -- define vim.g.terminal_color_{0,17}
 --    colors = {                   -- add/modify theme and palette colors
 --        palette = {},
---        theme = { wave = {}, lotus = {}, wave = {}, all = {} },
+--        theme = { zen = {}, pearl = {}, ink = {}, all = {} },
 --    },
 --    overrides = function(colors) -- add/modify highlights
 --        return {}
 --    end,
---    theme = "wave",              -- Load "wave" theme when 'background' option is not set
+--    theme = "zen",              -- Load "zen" theme
 --    background = {               -- map the value of 'background' option to a theme
---        dark = "wave",           -- try "wave" !
---        light = "lotus"
+--        dark = "ink",           -- try "ink" !
+--        light = "pearl"
 --    },
 --})
 --
 ---- setup must be called before loading
---vim.cmd("colorscheme kanagawa")
+--vim.cmd("colorscheme kanso")
