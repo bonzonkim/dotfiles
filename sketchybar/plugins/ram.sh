d91ab6fe492dcfbfba35f967ff67b40148acaba8
Bumgu Kang <flrhyme9@gmail.com>
2025-12-23 18:10:58 +0900

    dotfiles(sketchybar)

--- sketchybar/plugins/ram.sh
+++ sketchybar/plugins/ram.sh
@@ -9,16 +9,16 @@ PAGE_SIZE=$(pagesize)
 VM_STAT=$(vm_stat)
 PAGES_ACTIVE=$(echo "$VM_STAT" | grep "Pages active" | awk '{print $3}' | tr -d '.')
 PAGES_WIRED=$(echo "$VM_STAT" | grep "Pages wired down" | awk '{print $4}' | tr -d '.')
-PAGES_COMPRESSED=$(echo "$VM_STAT" | grep "Pages stored in compressor" | awk '{print $5}' | tr -d '.')
+PAGES_COMPRESSOR=$(echo "$VM_STAT" | grep "Pages occupied by compressor" | awk '{print $5}' | tr -d '.')
 
-# Calculate used memory in bytes
-USED_PAGES=$((PAGES_ACTIVE + PAGES_WIRED + PAGES_COMPRESSED))
-USED_BYTES=$((USED_PAGES * PAGE_SIZE))
-
-# Get total memory in bytes
+# Get total memory
 TOTAL_BYTES=$(sysctl -n hw.memsize)
+TOTAL_PAGES=$((TOTAL_BYTES / PAGE_SIZE))
+
+# Calculate used memory (Active + Wired + Compressor) - matches RunCat/Activity Monitor
+USED_PAGES=$((PAGES_ACTIVE + PAGES_WIRED + PAGES_COMPRESSOR))
 
 # Calculate percentage
-PERCENTAGE=$((USED_BYTES * 100 / TOTAL_BYTES))
+PERCENTAGE=$((USED_PAGES * 100 / TOTAL_PAGES))
 
 sketchybar --set "$NAME" label="${PERCENTAGE}%" label.color="$WHITE"
