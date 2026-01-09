f5f310814bffa81ac8f1c6a82bdfeb9269ea0090
bonzonkim <flrhyme9@gmail.com>
2026-01-09 22:51:01 +0900

    dotfiles: use pre-build for markdown-preview.nvim

--- nvim/lua/lazy-setup.lua
+++ nvim/lua/lazy-setup.lua
@@ -137,7 +137,9 @@ require("lazy").setup({
   {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && yarn install",
+  build = function()
+    vim.fn["mkdp#util#install"]()
+  end,
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
   end,
