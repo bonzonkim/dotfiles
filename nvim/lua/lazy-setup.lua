d9f5c625bc8faca67e8c4c9f3434772f8006b210
bonzonkim <flrhyme9@gmail.com>
2026-03-16 13:18:21 +0900

    dotfiles(nvim): 99 ai agent

--- nvim/lua/lazy-setup.lua
+++ nvim/lua/lazy-setup.lua
@@ -161,6 +161,36 @@ require("lazy").setup({
   --  require("strudel").setup()
   --end
   --},
+  {
+    "ThePrimeagen/99",
+      config = function()
+        local _99 = require("99")
+
+        _99.setup({
+          provider = _99.Providers.OpenCodeProvider,
+
+          model = "openrouter/anthropic/claude-sonnet-4.6",
+          logger = {
+            level = _99.DEBUG,
+            path = "/tmp/99.log",
+          },
+
+          tmp_dir = "./tmp",
+        })
+        vim.keymap.set("v", "<leader>9v", function()
+				_99.visual()
+        end)
+
+        --- if you have a request you dont want to make any changes, just cancel it
+        vim.keymap.set("n", "<leader>9x", function()
+          _99.stop_all_requests()
+        end)
+
+        vim.keymap.set("n", "<leader>9s", function()
+          _99.search()
+        end)
+      end
+  }
 },
 {
   install = { colorscheme = { "tokyonight" } },
