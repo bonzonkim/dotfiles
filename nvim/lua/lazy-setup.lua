9eaab6deef501540cdcefd36352ff3c0dfde51ad
bonzonkim <flrhyme9@gmail.com>
2026-03-24 10:42:09 +0900

    dotfile

--- nvim/lua/lazy-setup.lua
+++ nvim/lua/lazy-setup.lua
@@ -190,6 +190,15 @@ require("lazy").setup({
           _99.search()
         end)
       end
+  },
+  {
+    'nvim-flutter/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = true,
   }
 },
 {
